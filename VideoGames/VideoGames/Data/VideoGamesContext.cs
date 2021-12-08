8afe75658679336e01876e267004f5bbe7daf741
Paxton Lee <70034423+PaxtonLee@users.noreply.github.com>
2021-12-08 17:23:32 -0600

    Added Model for Games

---
+++ VideoGames/VideoGames/Data/VideoGamesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VideoGames.Models;
+
+namespace VideoGames.Data
+{
+    public class VideoGamesContext : DbContext
+    {
+        public VideoGamesContext (DbContextOptions<VideoGamesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VideoGames.Models.Game> Game { get; set; }
+    }
+}
---
+++ VideoGames/VideoGames/Pages/Games/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VideoGames.Data;
+using VideoGames.Models;
+
+namespace VideoGames.Pages.Games
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly VideoGames.Data.VideoGamesContext _context;
+
+        public DetailsModel(VideoGames.Data.VideoGamesContext context)
+        {
+            _context = context;
+        }
+
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
